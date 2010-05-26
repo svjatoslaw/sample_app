c5949bb1a26cd9df837ad11952d86105e2221a48
svjatoslaw <svjatoslaw.inbbox@gmail.com>
2010-05-26 10:02:38 +0400

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '36abff264db21e1d2e17106c3ec9a6be840f6942f5ceae26882f7936195923e93be9edfdbabffc116bdef5e923eed338aa8971c73c413b829dc2bc0e39c4d9c3';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_sample_app_session',
+  :secret      => '63307832e0814c8058853b5ec66d7cb9037ef0424798d28196e8be7e1478a0833572d6d5a67e4930bc895a9f9d73d06461b6b576166aa08584d046995bb2df6a'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
